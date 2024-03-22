c2e36373f3571fb5e7e048095012b8f6cbe65017
admin <slava.gusaov@gmail.com>
2024-03-22 17:58:41 +0300

    Initial commit

---
+++ src/main/java/bank/Bank.java
@@ -0,0 +1,30 @@
+package bank;
+
+import data.BankAccount;
+import data.Client;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private static List<BankAccount> accounts;
+
+
+    private static final String ADMIN_LOGIN = "admin";
+    private static final String ADMIN_PASSWORD = "0000";
+
+
+    static {
+        accounts = new ArrayList<>();
+
+        Client client = new Client("Andrew", "Johnson");
+        BankAccount bankAccount = new BankAccount(client);
+
+        accounts.add(bankAccount);
+    }
+
+    public static List<BankAccount> getAccounts() {
+        return accounts;
+    }
+}
